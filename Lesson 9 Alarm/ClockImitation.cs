42578485f5b848a1d79fd975c7c97313cae4ad8d
Danielzhel <fartunazh@gmail.com>
2022-11-10 14:14:00 +0300

    Добавьте файлы проекта.

---
+++ Lesson 9 Alarm/ClockImitation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_9_Alarm
+{
+    public class ClockImitation
+    {
+        public delegate void ActionComplite();
+
+        private string _userMessage { get; set; }
+        private int _time { get; set; }
+
+        public ClockImitation(int time, string userMessage)
+        {
+            Console.WriteLine(userMessage);
+            _time = time;
+        }
+
+        public void Alarm()
+        {
+            Thread.Sleep(_time*1000);
+            action.Invoke();
+        }
+
+        public static void EventMessage()
+        {
+            Console.WriteLine("Time is runnig out!");
+        }
+
+        public ActionComplite action = new ActionComplite(EventMessage);
+
+    }
+
+    public class Observer
+    {
+        public void AddObsreverNotification(ClockImitation alarmNum)
+        {
+            alarmNum.action += ObserverMessage;
+        }
+        
+        public void ObserverMessage()
+        {
+            Console.WriteLine("I'm Observer. I got the message about ending");
+        }
+    }
+}
---
+++ Lesson 9 Alarm/Program.cs
@@ -0,0 +1,7 @@
+﻿using Lesson_9_Alarm;
+
+ClockImitation Alarm1 = new ClockImitation(3, "I'm first user. And I start first Alarm. ");
+Observer FirstObserver = new Observer();
+FirstObserver.AddObsreverNotification(Alarm1);
+
+Alarm1.Alarm();
